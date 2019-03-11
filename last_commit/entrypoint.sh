67b181435256bc36933e174b11b7d3165d52497c
Florian Keller <github@floriankeller.de>
2019-03-11 12:03:53 +0100

    fix: Grep only first line of last commit

--- last_commit/entrypoint.sh
+++ last_commit/entrypoint.sh
@@ -5,7 +5,7 @@
 PATTERN="$*"
 LAST_COMMIT="$(git log -1 --pretty=%B)"
 
-if echo "$LAST_COMMIT" | grep -qP "$PATTERN"; then
+if echo "$LAST_COMMIT" | head -n 1 | grep -qP "$PATTERN"; then
   echo "\"$LAST_COMMIT\" matches \"$PATTERN\""
   exit 0
 fi
