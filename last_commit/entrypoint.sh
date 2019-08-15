5ed75ae4aca0adf47d3b78daff105dc69c2b975d
Florian Keller <github@floriankeller.de>
2019-08-15 20:19:26 +0200

    feat(last_commit): Rewrite for workflow 2

--- last_commit/entrypoint.sh
+++ last_commit/entrypoint.sh
@@ -5,7 +5,8 @@
 set -e
 
 PATTERN="$*"
-LAST_COMMIT="$(git log -1 --pretty=%B | head -n 1)"
+GIT_LAST_COMMIT="$(git log -1 --pretty=%B | head -n 1)"
+LAST_COMMIT=${EVENT_COMMIT_MESSAGE:GIT_LAST_COMMIT}
 
 if echo "$LAST_COMMIT" | grep -qP "$PATTERN"; then
   echo "\"$LAST_COMMIT\" matches \"$PATTERN\""
